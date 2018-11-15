cb3b67a83a887cf7a7447b70935728044fd9be68
Andy Lindsay <andydlindsay@gmail.com>
2018-11-15 02:30:07 +0000

    adds order routes

--- routes/orders.js
+++ routes/orders.js
@@ -5,10 +5,22 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
-  router.get("/", (req, res) => {
+  router.get("/:id/menu_items", (req, res) => {
     knex
-      .select("*")
+      .from("order_menu_items")
+      .join("menu_items", "menu_items.id", "order_menu_items.menu_item_id")
+      .select("menu_items.name", "menu_items.price", "menu_items.description", "menu_items.image_url", "order_menu_items.comment")
+      .where({ "order_menu_items.order_id": req.params.id })
+      .then((results) => {
+        res.json(results);
+      });
+  });
+  
+  router.get("/:id", (req, res) => {
+    knex
+      .select("pickup_time", "accepted", "ready")
       .from("orders")
+      .where({ id: req.params.id })
       .then((results) => {
         res.json(results);
     });
