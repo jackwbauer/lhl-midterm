1a2a330267042f7d8fe9058389c8ced196b87ed4
Andy Lindsay <andydlindsay@gmail.com>
2018-11-17 15:19:33 -0800

    styles location list page

--- db/seeds/c_locations.js
+++ db/seeds/c_locations.js
@@ -5,7 +5,7 @@ exports.seed = function (knex, Promise) {
     return knex('locations').del()
         .then(function () {
             return Promise.all([
-                knex('locations').insert({
+                knex('locations').insert([{
                     id: 1,
                     restaurant_id: 1,
                     address: '123 Airship Way',
@@ -17,7 +17,31 @@ exports.seed = function (knex, Promise) {
                     thursday: '9AM - 9PM',
                     friday: '9AM - 9PM',
                     saturday: '10AM - 10PM',
-                }),
+                }, {
+                    id: 2,
+                    restaurant_id: 1,
+                    address: '1400 Aeronautic Blvd',
+                    phone: phone1,
+                    sunday: 'Closed',
+                    monday: '8AM - 4PM',
+                    tuesday: '8AM - 4PM',
+                    wednesday: '8AM - 4PM',
+                    thursday: '8AM - 4PM',
+                    friday: '8AM - 4PM',
+                    saturday: '8AM - 6PM'
+                }, {
+                    id: 3,
+                    restaurant_id: 1,
+                    address: '420 Helium Drive',
+                    phone: phone1,
+                    sunday: '2PM - 6PM',
+                    monday: '10AM - 6PM',
+                    tuesday: '10AM - 6PM',
+                    wednesday: '10AM - 6PM',
+                    thursday: '10AM - 6PM',
+                    friday: '10AM - 6PM',
+                    saturday: '10AM - 2PM'
+                }])
             ]);
         });
 };
