47b58ce93170345ba148cb5978c31376fc909be4
Andy Lindsay <andydlindsay@gmail.com>
2018-11-17 16:13:23 -0800

    fixes time function

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -3,29 +3,26 @@
 const express = require('express');
 const router  = express.Router();
 
-function getCurrentDayHours(knexResult) {
+function getCurrentDay() {
   const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
   const day = new Date().getDay();
-  const hours = `${knexResult[days[day] + '_opening']} - ${knexResult[days[day] + '_closing']}`;
-  return hours;
+  console.log(day);
+  return days[day];
 }
 
 module.exports = (knex) => {
 
   router.get('/:id/locations', (req, res) => {
+    const day = getCurrentDay();
     knex
-      .select('res.name', 'loc.address', 'loc.phone', 'loc.sunday_opening', 'loc.sunday_closing', 'loc.monday_opening', 'loc.monday_closing', 'loc.tuesday_opening', 'loc.tuesday_closing', 'loc.wednesday_opening', 'loc.wednesday_closing', 'loc.thursday_opening', 'loc.thursday_closing', 'loc.friday_opening', 'loc.friday_closing', 'loc.saturday_opening', 'loc.saturday_closing', 'loc.id')
+      .select('res.name', 'loc.address', 'loc.phone', `loc.${day}_opening as opening`, `loc.${day}_closing as closing`, 'loc.id')
       .from('locations as loc')
       .join('restaurants as res', 'loc.restaurant_id', 'res.id')
       .where('loc.active', true)
       .andWhere('res.id', req.params.id)
       .then((results) => {
-        const returnArray = results.map((elem) => {
-          elem.hours = getCurrentDayHours(elem);
-          return elem;
-        });
         const templateVars = {
-          locations: returnArray
+          locations: results
         };
         res.render('location_list', templateVars);
       });
