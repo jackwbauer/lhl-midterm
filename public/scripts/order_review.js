49b51ff0d105e501f5c0882b6aee99791fb960f9
Andy Lindsay <andydlindsay@gmail.com>
2018-11-18 15:50:55 -0800

    adds order confirmation template

--- public/scripts/order_review.js
+++ public/scripts/order_review.js
@@ -2,20 +2,14 @@ $(() => {
 
     const order_id = window.location.href.split('/')[4];
 
-    function handleCommentChange(event) {
-        $comment = $(event.target);
-
-    }
-
-    $('input.comment').change(handleCommentChange);
-
     $('#confirm').click(event => {
         $.ajax({
             url: `/orders/${order_id}/review?_method=PUT`,
             type: 'PUT',
             success: function(data) {
-                window.location.replace('/');
+                window.location.replace(`/orders/${order_id}/confirmation`);
             }
-        })
-    })
+        });
+    });
+
 });
