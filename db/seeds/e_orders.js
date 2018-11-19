81a1bceefe74456d1a549f468802aa6545986924
Andy Lindsay <andydlindsay@gmail.com>
2018-11-18 16:41:11 -0800

    adds order seeds

--- db/seeds/e_orders.js
+++ db/seeds/e_orders.js
@@ -2,12 +2,24 @@ exports.seed = function (knex, Promise) {
     return knex('orders').del()
       .then(function () {
         return Promise.all([
-          knex('orders').insert({
+          knex.into('orders').insert([{
             id: 1,
             user_id: 2,
             location_id: 1,
             comment: 'I WANT MY FOOD NOW!!!'
-          }),
+          }, {
+            id: 2,
+            user_id: 3,
+            location_id: 1
+          }, {
+            id: 3,
+            user_id: 4,
+            location_id: 1
+          }, {
+            id: 4,
+            user_id: 5,
+            location_id: 1
+          }]),
         ]);
       });
   };
--- db/seeds/f_order_menu_items.js
+++ db/seeds/f_order_menu_items.js
@@ -2,24 +2,55 @@ exports.seed = function (knex, Promise) {
     return knex('order_menu_items').del()
         .then(function () {
             return Promise.all([
-                knex('order_menu_items').insert({
+                knex.into('order_menu_items').insert({
                     id: 1,
                     order_id: 1,
                     menu_item_id: 1,
                     comment: 'FISH!'
                 }),
-                knex('order_menu_items').insert({
+                knex.into('order_menu_items').insert({
                     id: 2,
                     order_id: 1,
                     menu_item_id: 4,
                     comment: 'Boil em, mash em, put em in a stew'
                 }),
-                knex('order_menu_items').insert({
+                knex.into('order_menu_items').insert([{
                     id: 3,
                     order_id: 1,
                     menu_item_id: 6,
                     comment: 'STEAK!'
-                }),
+                }, {
+                    id: 4,
+                    order_id: 2,
+                    menu_item_id: 2,
+                    comment: 'Hold the Mayo!'
+                }, {
+                    id: 5,
+                    order_id: 2,
+                    menu_item_id: 2
+                }, {
+                    id: 6,
+                    order_id: 2,
+                    menu_item_id: 6,
+                    comment: 'Please don\'t burn it this time...'
+                }, {
+                    id: 7,
+                    order_id: 3,
+                    menu_item_id: 2
+                }, {
+                    id: 8,
+                    order_id: 3,
+                    menu_item_id: 5
+                }, {
+                    id: 9,
+                    order_id: 4,
+                    menu_item_id: 1,
+                    comment: 'Mmmm can\'t wait!'
+                }, {
+                    id: 10,
+                    order_id: 4,
+                    menu_item_id: 5
+                }]),
             ]);
         });
 };
